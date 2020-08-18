cc8c240b501cdc2a727cbdd37e9a5f83bb282841
KalinaAnn <57825196+KalinaAnn@users.noreply.github.com>
2020-08-18 22:00:42 +0800

    Layout Updates

--- User/app/src/main/java/com/example/click/Splashscreen.java
+++ User/app/src/main/java/com/example/click/Splashscreen.java
@@ -32,6 +32,7 @@ public class Splashscreen extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
                 overridePendingTransition(R.anim.fadein, R.anim.fadeout);
             }
