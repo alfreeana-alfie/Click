85442806e614794a48fb234c56f497f0e863918a
KalinaAnn <57825196+KalinaAnn@users.noreply.github.com>
2021-03-11 15:12:26 +0800

    Update

--- User/app/src/main/java/com/ketekmall/ketekmall/pages/Homepage.java
+++ User/app/src/main/java/com/ketekmall/ketekmall/pages/Homepage.java
@@ -604,20 +604,9 @@ public class Homepage extends AppCompatActivity {
                 GotoCategory(1);
             }
         });
-        ImageProcess.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GotoCategory(1);
-            }
-        });
+        ImageProcess.setOnClickListener();
 
         button_handcraft.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GotoCategory(2);
-            }
-        });
-        ImageHandicraft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 GotoCategory(2);
@@ -630,20 +619,8 @@ public class Homepage extends AppCompatActivity {
                 GotoCategory(6);
             }
         });
-        ImageHealth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GotoCategory(6);
-            }
-        });
 
         button_home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GotoCategory(7);
-            }
-        });
-        ImageHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 GotoCategory(7);
@@ -656,20 +633,8 @@ public class Homepage extends AppCompatActivity {
                 GotoCategory(8);
             }
         });
-        ImageFashion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GotoCategory(8);
-            }
-        });
 
         button_pepper.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GotoCategory(9);
-            }
-        });
-        ImageSarawak.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 GotoCategory(9);
